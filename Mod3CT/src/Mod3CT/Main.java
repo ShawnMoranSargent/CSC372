99fe6ec020099922e5ce0568dcb56dcc0b36fcfd
Shawn Moran-Sargent <Shawn.moransargent@csuglobal.edu>
2024-12-04 22:41:58 -0700

    Started Module3 critical think

---
+++ Mod3CT/src/Mod3CT/Main.java
@@ -0,0 +1,25 @@
+package Mod3CT;
+
+import javafx.scene.paint.Color;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("hellofx.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 400, 300));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ module3criticalthink/src/main/java/org/openjfx/App.java
@@ -0,0 +1,64 @@
+package org.openjfx;
+
+import java.util.Date;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        primaryStage.setTitle("User Interface I");
+        Button TimeInfo = new Button("Date and Time");
+        Button TextToFile = new Button("Submit log.txt");
+        Button RandGreen = new Button("Random Green");
+        Button Exit = new Button("Exit Program");
+
+        Text DateTime = new Text();
+        TextArea LogInput = new TextArea("Type in me!");
+
+        HBox Menu = new HBox(TimeInfo, TextToFile, RandGreen, Exit);
+        Menu.setAlignment(Pos.CENTER);
+
+        BorderPane baseLayout = new BorderPane();
+        baseLayout.setPadding(new Insets(5,5,5,5));
+        baseLayout.setTop(Menu);
+        baseLayout.setCenter(LogInput);
+        baseLayout.setBottom(DateTime);
+
+        TimeInfo.setOnAction(event -> {
+            Date currDate= new Date();
+            
+            DateTime.setText(currDate.toString());
+        });
+        TextToFile.setOnAction(event -> {
+            
+        });
+        RandGreen.setOnAction(event -> {
+            
+        });
+        Exit.setOnAction(event -> {
+            
+        });
+        Scene scene = new Scene(baseLayout, 400,100);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+}
